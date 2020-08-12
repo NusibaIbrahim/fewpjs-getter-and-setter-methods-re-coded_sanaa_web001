7ca57830b90e700af6f740b505b69ae1ae45255b
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-12 12:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Circle {
     return this.radius * 2;
   }
   get circumference() {
-    return Math.pi * this.diameter;
+    return Math.pi * this.radius * 2;
   }
   get area() {
     return Math.pi * this.radius * this.radius;
