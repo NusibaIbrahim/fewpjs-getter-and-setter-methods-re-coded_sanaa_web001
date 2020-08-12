ddb0d552c6d0228c520c83adddeb22877eb36c0c
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-12 12:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+let circle;
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  get diameter() {
+    return Diameter;
+  }
+  get circumference() {
+    return Circumference ;
+  }
+  get area() {
+    return Area;
+  }
+  set diameter(){
+    Diameter = this.radius * 2
+  }
+  set circumference(){
+    Circumference = math.pi * this.Diameter
+  }
+  set area() {
+    Area = math.pi * this.radius * this.radius;
+}
+circle = new Circle(6);
+circle.diameter ;
+circle.diameter = 36;
+circle.circumference;
+circle.circumference = 100;
